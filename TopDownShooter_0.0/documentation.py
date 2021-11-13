ee4731c23f61be36dae6f6cac03c0af3cabd1eca
Wildric-Auric <70033490+Wildric-Auric@users.noreply.github.com>
2021-11-14 00:33:28 +0100

    trying to make doc in a separate file

---
+++ TopDownShooter_0.0/documentation.py
@@ -0,0 +1,12 @@
+import main
+main.GprintAfter.__doc__ =  '''
+        function type----
+
+        generator
+
+        arguments-----
+        idd: the id for coroutine manager
+        text: type string, the string to be printed
+        time: float representing seconds after wich the text is printed
+        Note that this generator should be passed as first argument to the function StartCoroutine
+        '''
