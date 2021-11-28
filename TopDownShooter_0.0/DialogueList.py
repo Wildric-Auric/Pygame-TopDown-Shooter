60b125afcf21eb4e45a1f7fddf2e65cf7336a36f
Wildric-Auric <70033490+Wildric-Auric@users.noreply.github.com>
2021-11-28 15:53:38 +0100

    fixed endline, added separate file for dialogues

---
+++ TopDownShooter_0.0/DialogueList.py
@@ -0,0 +1,6 @@
+SET1TEST = [
+    
+    "Wake up Neo.<W:0.1>.<W:0.1>.", 
+    "<FONT:Alice><COLOR:GREEN>The Matrix<COLOR:WHITE> has you...", 
+    "Follow then <COLOR:YELLOW><S:0.5,10,0>w<S:0.5,10,1>h<S:0.5,10,2>i<S:0.5,10,3>t<S:0.5,10,4>e<S:0.5,10,5><COLOR:WHITE><S:0,0,0>\nrabbit..."
+                          ]
